cd4b985a9469824d021af98351269adc4583aea3
FreyzS7 <abanggaul111@gmail.com>
2025-08-18 10:34:51 +0700

    fix eslint5

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,7 +19,8 @@ const eslintConfig = [
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-require-imports": "off",
       "@typescript-eslint/no-this-alias": "off",
-      "@typescript-eslint/no-unused-expressions": "off"
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/no-explicit-any": "off"
     }
   },
   {
@@ -29,7 +30,8 @@ const eslintConfig = [
       "@typescript-eslint/no-require-imports": "off",
       "@typescript-eslint/no-empty-object-type": "off",
       "@typescript-eslint/no-this-alias": "off",
-      "@typescript-eslint/no-unused-expressions": "off"
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/no-explicit-any": "off"
     }
   }
 ];
