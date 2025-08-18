a3bdb7bf43cf875218992b3b41eecec2d9a9e720
FreyzS7 <abanggaul111@gmail.com>
2025-08-18 10:23:05 +0700

    fix eslint2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,16 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-require-imports": "off"
+    }
+  },
+  {
+    files: ["src/generated/**/*"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-require-imports": "off",
       "@typescript-eslint/no-empty-object-type": "off"
     }
   }
